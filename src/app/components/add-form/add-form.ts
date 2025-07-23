d384a9a646b663e290942c0a698f6129380ddb11
parsalemi <parsalemi@outlook.com>
2025-07-23 08:53:23 +0330

    init: generate components and services

---
+++ src/app/components/add-form/add-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-form',
+  imports: [],
+  templateUrl: './add-form.html',
+  styleUrl: './add-form.scss'
+})
+export class AddForm {
+
+}
