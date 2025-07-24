cb1809187a1c19f498a36d95e914e9ab17ad8008
parsalemi <parsalemi@outlook.com>
2025-07-24 13:05:09 +0330

    add: basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./components/dashboard/dashboard').then(c => c.Dashboard),
+  },
+  {
+    path: 'my-list',
+    loadComponent: () => import('./components/list/list').then(c => c.List),
+  },
+  {
+    path: 'add-new',
+    loadComponent: () => import('./components/add-form/add-form').then(c => c.AddForm),
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./components/settings/settings').then(c => c.Settings),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./components/register/register').then(c => c.Register),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login').then(c => c.Login),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./components/not-found/not-found').then(c => c.NotFound),
+  }
+];
