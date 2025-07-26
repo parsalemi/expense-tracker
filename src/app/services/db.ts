134973ed36e339045f100eafb3f333bfe774d06c
parsalemi <parsalemi@outlook.com>
2025-07-26 09:38:41 +0330

    implement: db service

--- src/app/services/db.ts
+++ src/app/services/db.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import Dexie from 'dexie';
+import { Transaction } from '../models/transaction.mode';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Db {
-  
+export class Db extends Dexie{
+  transactions!: Dexie.Table<Transaction, number>;
+
+  constructor() {
+    super('expensesDB');
+
+    this.version(1).stores({
+      transactions: '++id, type, date',
+    });
+  }
+
+  async deleteData() : Promise<void> {
+    try {
+      await this.delete();
+    } catch (error) {
+      console.error('An error occured: ', error)
+    }
+  }
 }
